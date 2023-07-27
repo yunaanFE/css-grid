f1455b95bac4f3934f13d3b64ee91ffc21e7dc0f
yunaan <yunaan@mymusictaste.com>
2023-07-27 13:09:13 +0900

    update favicon

--- app/Layout.tsx
+++ app/Layout.tsx
@@ -4,7 +4,7 @@ import styles from './Layout.module.scss';
 const Layout = () => {
   return (
     <div className={styles.container}>
-        <CssGrid />
+      <CssGrid />
     </div>
   );
 };
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,3 +1,20 @@
-import Document from 'next/document';
+import { Head, Html, Main, NextScript } from 'next/document';
 
-export default class MyDocument extends Document {}
+export default function Document() {
+  return (
+    <Html lang="en">
+      <Head>
+        <meta name="theme-color" content="#e74c3c" />
+        <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/favicon/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon/favicon-16x16.png" />
+        <link rel="manifest" href="/favicon/manifest.json" />
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+}
